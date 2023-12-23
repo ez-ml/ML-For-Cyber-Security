37ead1c91f879bd4d82809da488b99608edf2d92
SHAILESH PILARE <shailesh.pilare@gmail.com>
2023-12-23 12:17:13 -0800

    Add files via upload

---
+++ project/llama_2_7b_single_gpu.py
@@ -0,0 +1,111 @@
+import logging
+import os
+import yaml
+import numpy as np; np.random.seed(123)
+import pandas as pd
+
+
+from ludwig.api import LudwigModel
+
+os.environ["HUGGING_FACE_HUB_TOKEN"]="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"
+
+df = pd.read_json("https://raw.githubusercontent.com/sahil280114/codealpaca/master/data/code_alpaca_20k.json")
+
+total_rows = len(df)
+split_0_count = int(total_rows * 0.9)
+split_1_count = int(total_rows * 0.05)
+split_2_count = total_rows - split_0_count - split_1_count
+
+# Create an array with split values based on the counts
+split_values = np.concatenate([
+    np.zeros(split_0_count),
+    np.ones(split_1_count),
+    np.full(split_2_count, 2)
+])
+
+# Shuffle the array to ensure randomness
+np.random.shuffle(split_values)
+
+# Add the 'split' column to the DataFrame
+df['split'] = split_values
+df['split'] = df['split'].astype(int)
+
+# For this webinar, we will just 500 rows of this dataset.
+df = df.head(n=1000)
+
+
+
+
+qlora_fine_tuning_config = yaml.safe_load(
+"""
+model_type: llm
+base_model: meta-llama/Llama-2-7b-hf
+
+input_features:
+  - name: instruction
+    type: text
+
+output_features:
+  - name: output
+    type: text
+
+prompt:
+  template: >-
+    Below is an instruction that describes a task, paired with an input
+    that provides further context. Write a response that appropriately
+    completes the request.
+
+    ### Instruction: {instruction}
+
+    ### Input: {input}
+
+    ### Response:
+
+generation:
+  temperature: 0.1
+  max_new_tokens: 512
+
+adapter:
+  type: lora
+
+quantization:
+  bits: 4
+
+preprocessing:
+  global_max_sequence_length: 512
+  split:
+    type: random
+    probabilities:
+    - 1
+    - 0
+    - 0
+
+trainer:
+  type: finetune
+  epochs: 1
+  batch_size: 1
+  eval_batch_size: 2
+  gradient_accumulation_steps: 16
+  learning_rate: 0.0004
+  learning_rate_scheduler:
+    warmup_fraction: 0.03
+
+backend:
+  type: ray
+  trainer:
+    use_gpu: true
+    strategy:
+      type: deepspeed
+      zero_optimization:
+        stage: 3
+        offload_optimizer:
+          device: cpu
+          pin_memory: true
+"""
+)
+
+model = LudwigModel(config=qlora_fine_tuning_config, logging_level=logging.INFO)
+results = model.train(dataset=df)
+
+
+
